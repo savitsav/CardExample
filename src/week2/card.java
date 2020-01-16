e503d3f590c2430e2257625f5735ce83ff4dce1b
savitsav <savitsav@sheridancollege.ca>
2020-01-16 11:39:29 -0500

    first Commit

---
+++ src/week2/card.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week2;
+
+/**
+ *
+ * @author User
+ */
+public class card {
+    private String suit;
+    
+    private int value;
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+    
+    
+}
